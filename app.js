566ed57e38e26f4c491827b603784188c75501a6
Irene <grim6ace@gmail.com>
2024-10-01 02:32:55 +0300

    commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const numbers = [...document.querySelectorAll('.number')];
+
+const updateCount = (element) => {
+  const value = parseInt(element.dataset.value);
+  const increment = Math.ceil(value / 1000);
+  let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+
+    if (initialValue > value) {
+      element.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+
+    element.textContent = `${initialValue}+`;
+  }, 1);
+};
+
+numbers.forEach((number) => {
+  updateCount(number)
+});
